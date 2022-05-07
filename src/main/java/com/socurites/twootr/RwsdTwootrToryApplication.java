8ce29bf2eadae16a909c44b74d8b199019a2a646
aaronsleepy <aaron@kmong.com>
2022-05-07 22:37:04 +0900

    first commit

---
+++ src/main/java/com/socurites/twootr/RwsdTwootrToryApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.twootr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsdTwootrToryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwsdTwootrToryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/socurites/twootr/RwsdTwootrToryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socurites.twootr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwsdTwootrToryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
